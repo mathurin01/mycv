255e94d3419cb4b3d80de77e89f9151c6e6effc0
mathurin <jpetiteau@gmail.com>
2015-05-04 13:52:59 +0200

    insertion de l'itération 7

--- src/DAO/CategoryDAO.php
+++ src/DAO/CategoryDAO.php
@@ -2,27 +2,10 @@
 
 namespace Mycv\DAO;
 
-use Doctrine\DBAL\Connection;
 use Mycv\Domain\Category;
 
-class CategoryDAO
+class CategoryDAO extends DAO
 {
-    /**
-     * Database connection
-     *
-     * @var \Doctrine\DBAL\Connection
-     */
-    private $db;
-
-    /**
-     * Constructor
-     *
-     * @param \Doctrine\DBAL\Connection The database connection object
-     */
-    public function __construct(Connection $db) {
-        $this->db = $db;
-    }
-
     /**
      * Return a list of all categories, sorted by date (most recent first).
      *
@@ -30,13 +13,13 @@ class CategoryDAO
      */
     public function findAll() {
         $sql = "select * from t_category order by cat_id desc";
-        $result = $this->db->fetchAll($sql);
+        $result = $this->getDb()->fetchAll($sql);
         
         // Convert query result to an array of domain objects
         $categories = array();
         foreach ($result as $row) {
             $categoryId = $row['cat_id'];
-            $categories[$categoryId] = $this->buildCategory($row);
+            $categories[$categoryId] = $this->buildDomainObject($row);
         }
         return $categories;
     }
@@ -47,7 +30,8 @@ class CategoryDAO
      * @param array $row The DB row containing Category data.
      * @return \Mycv\Domain\Category
      */
-    private function buildCategory(array $row) {
+    protected function buildDomainObject($row) {
+
         $category = new Category();
         $category->setId($row['cat_id']);
         $category->setTitle($row['cat_title']);
