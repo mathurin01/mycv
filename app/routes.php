a953a32423b0a0b240c6e2f0af803d4bddf0fb3a
mathurin <jpetiteau@gmail.com>
2015-05-04 15:35:08 +0200

    itération 8 terminée

--- app/routes.php
+++ app/routes.php
@@ -1,18 +1,32 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 // Home page
 $app->get('/', function () use ($app) {
     $articles = $app['dao.article']->findAll();    
-//    return $app['twig']->render('index.html.twig', array('articles' => $articles));
-
     $categories = $app['dao.category']->findAll();
+
     return $app['twig']->render('index.html.twig', array(
 	'articles' => $articles,
-	'categories' => $categories
-    ));
+	'categories' => $categories));
+    });
 
-    ob_start();             // start buffering HTML output
-    require '../views/view.php';
-    $view = ob_get_clean(); // assign HTML output to $view
-    return $view;
-});
+// Detailed info about an article
+$app->get('/article/{id}', function ($id) use ($app) {
+    $article = $app['dao.article']->find($id);
+    $comments = $app['dao.comment']->findAllByArticle($id);
+    return $app['twig']->render('article.html.twig', array(
+	'article' => $article, 
+	'comments' => $comments));
+    });
+
+// Login form
+$app->get('/login', function(Request $request) use ($app) {
+    return $app['twig']->render('login.html.twig', array(
+        'error'         => $app['security.last_error']($request),
+        'last_username' => $app['session']->get('_security.last_username'),
+    ));
+})->bind('login');  // named route so that path('login') works in Twig templates
+
+
--- db/content.sql
+++ db/content.sql
@@ -5,6 +5,13 @@ insert into t_article values
 insert into t_article values
 (3, 'Lorem ipsum in french', "J’en dis autant de ceux qui, par mollesse d’esprit, c’est-à-dire par la crainte de la peine et de la douleur, manquent aux devoirs de la vie. Et il est très facile de rendre raison de ce que j’avance. Car, lorsque nous sommes tout à fait libres, et que rien ne nous empêche de faire ce qui peut nous donner le plus de plaisir, nous pouvons nous livrer entièrement à la volupté et chasser toute sorte de douleur ; mais, dans les temps destinés aux devoirs de la société ou à la nécessité des affaires, souvent il faut faire divorce avec la volupté, et ne se point refuser à la peine. La règle que suit en cela un homme sage, c’est de renoncer à de légères voluptés pour en avoir de plus grandes, et de savoir supporter des douleurs légères pour en éviter de plus fâcheuses.");
 
+/* raw password is 'john' */
+insert into t_user values
+(1, 'JohnDoe', 'L2nNR5hIcinaJkKR+j4baYaZjcHS0c3WX2gjYF6Tmgl1Bs+C9Qbr+69X8eQwXDvw0vp73PrcSeT0bGEW5+T2hA==', 'YcM=A$nsYzkyeDVjEUa7W9K', 'ROLE_USER');
+/* raw password is 'jane' */
+insert into t_user values
+(2, 'JaneDoe', 'EfakNLxyhHy2hVJlxDmVNl1pmgjUZl99gtQ+V3mxSeD8IjeZJ8abnFIpw9QNahwAlEaXBiQUBLXKWRzOmSr8HQ==', 'dhMTBkzwDKxnD;4KNs,4ENy', 'ROLE_USER');
+
 insert into t_category values
 (1, 'HTML5', 'imgs/logo_html5.jpg');
 insert into t_category values
@@ -12,4 +19,8 @@ insert into t_category values
 insert into t_category values
 (3, 'Bootstrap', 'imgs/logo_bootstrap.jpg');
 
+insert into t_comment values
+(1, 'John Doe', 'Great! Keep up the good work.', 1, 1);
+insert into t_comment values
+(2, 'Ann Yone', "Thank you, I'll try my best.", 1, 2);
 
